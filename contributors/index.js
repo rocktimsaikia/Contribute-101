106b8206e15f9300c081fcd1a159e1ea9d5425d9
RocktimSaikia <srocktim61@gmail.com>
2019-10-08 10:04:08 -0700

    Major update on the website

---
+++ contributors/index.js
@@ -0,0 +1,149 @@
+module.exports.list = [
+  {
+    name: "Rocktim",
+    country: "India",
+    tech_stack: ["React", "Node", "Express"],
+    github_username: "RocktimSaikia",
+    twitter_username: "RocktimSaikia10"
+  },
+  {
+    name: "Wisnu Febriadi",
+    country: "Indonesia",
+    tech_stack: ["Flutter", "Kotlin", "Swift"],
+    github_username: "wfebriad",
+    twitter_username: "#"
+  },
+  {
+    name: "Danielle Lyons",
+    country: "US",
+    tech_stack: ["JavaScript", "Node", "Express"],
+    github_username: "drding00",
+    twitter_username: "danachew"
+  },
+  {
+    name: "Arty",
+    country: "UK",
+    tech_stack: ["React", "Redux", "Sequelize ORM"],
+    github_username: "artysidorenko",
+    twitter_username: "PJSmooth3"
+  },
+  {
+    name: "Ankur Kalambe",
+    country: "United Kingdom",
+    tech_stack: ["Github", "Robotics", "Matlab"],
+    github_username: "ankurkalambe",
+    twitter_username: "ankur_kalambe"
+  },
+  {
+    name: "Ly Chunvira",
+    country: "Cambodia",
+    tech_stack: ["Python", "Vue", "React"],
+    github_username: "#",
+    twitter_username: "#"
+  },
+  {
+    name: "UMUT EKERDIKER",
+    country: "UNITED KINGDOM",
+    tech_stack: [
+      "JavaScript",
+      "MATLAB Deep learning",
+      "Artificial Intelligence"
+    ],
+    github_username: "RealBigpunch",
+    twitter_username: "MeisterShill"
+  },
+  {
+    name: "Silver",
+    country: "Earth",
+    tech_stack: ["React", "Javascript", "Css"],
+    github_username: "smarter23",
+    twitter_username: "#"
+  },
+  {
+    name: "Anthony Kusuma",
+    country: "Indonesia",
+    tech_stack: ["ReactJS", "Kotlin", "Laravel"],
+    github_username: "anthonykusuma",
+    twitter_username: "actuallyaka"
+  },
+  {
+    name: "Shriyansh Jain",
+    country: "India",
+    tech_stack: ["ReactJS", "Machine Learning", "Competitive Coding"],
+    github_username: "Shriyansh-J",
+    twitter_username: "#"
+  },
+  {
+    name: "Nikhil Vaidyar",
+    country: "India",
+    tech_stack: ["ReactJS", "Golang", "Graphql"],
+    github_username: "nikzayn",
+    twitter_username: "NikhilVaidyar"
+  },
+  {
+    name: "Kee Magbanua",
+    country: "Philippines",
+    tech_stack: ["JavaScript", "React", "Sass"],
+    github_username: "samcode23",
+    twitter_username: "zxcmoisturizer"
+  },
+  {
+    name: "Nikhil Gautam",
+    country: "India",
+    tech_stack: ["ReactJS", "Machine Learning", "Qt"],
+    github_username: "nik-gautam",
+    twitter_username: "NikhilG30907544"
+  },
+  {
+    name: "Shynd",
+    country: "Norway",
+    tech_stack: ["C#", "WPF", "WCF"],
+    github_username: "Shynd",
+    twitter_username: "#"
+  },
+  {
+    name: "Matteo F",
+    country: "Italy",
+    tech_stack: ["Laravel", "Java", "Python"],
+    github_username: "matteoformenti",
+    twitter_username: "#"
+  },
+  {
+    name: "Aman Agarwal",
+    country: "India",
+    tech_stack: ["Competitive Programming", "React.Js", "MongoDB"],
+    github_username: "BeastMaster07",
+    twitter_username: "amanag_007"
+  },
+  {
+    name: "Thanate",
+    country: "Thailand",
+    tech_stack: ["JS", "Vue", "Ruby"],
+    github_username: "mugenboat",
+    twitter_username: "#"
+  },
+  {
+    name: "Jay Parmani",
+    country: "India",
+    tech_stack: ["NodeJS", "React Native", "Blockchain"],
+    github_username: "jsparmani",
+    twitter_username: "jsparmani"
+  },
+  {
+    name: "Aditya Kumar Singh",
+    country: "India",
+    tech_stack: ["Django", "Arduino", "Angular.js"],
+    github_username: "adityakumar2809",
+    twitter_username: "adityakumar2809"
+  }
+
+  /* Template : 
+  {
+    name: "",
+    country: "",
+    tech_stack: ["", "", ""],
+    github_username: "",
+    twitter_username: ""
+  },
+  */
+];
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const { list } = require("./contributors/index");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { List: list });
 });
 
 const port = process.env.PORT || 5000;
