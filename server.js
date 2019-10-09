a817926d48327d48a1047e7db1b5aaff3d2618c3
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-10-09 22:46:49 +0530

    :sparkles: Fixed webpack hot reloading issue

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const app = express();
 const { list } = require("./contributors/index");
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const config = require("./webpack.config");
 
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'dist',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+});
+
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
 const { list } = require("./contributors/index");
 
 module.exports = {
-  entry: "./views/index.ejs",
-  target: "web",
+  entry: [
+    "webpack-hot-middleware/client?reload=true",
+    path.join(__dirname, "views/index.ejs")
+  ],
   output: {
-    filename: "index.html",
-    publicPath: "/dist/",
-    path: path.join(__dirname, "dist")
+    filename: "[name].js",
+    publicPath: "/",
+    path: path.join(__dirname, "/dist/")
   },
 
+  devtool: "eval-source-map",
   mode: "development",
-  stats: { colors: true },
-	devtool: "eval",
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.ejs$/,
-        loader: 'ejs-loader',
-        query: {
-          List: list
-        }
-      }
-    ]
-  },
-
+  
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
@@ -55,19 +27,30 @@ module.exports = {
       }
     }),
 
-    new CopyPlugin([
-      {
-        from: path.join(__dirname, "public"),
-        to: path.join(__dirname, "dist")
-      }
-    ])
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
 
-   devServer: {
-    contentBase: "dist",
-    port: 9000,
-    hot: true,
-    watchContentBase: true,
-    historyApiFallback: true
-  }
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.ejs$/,
+        loader: "ejs-loader"
+      }
+    ]
+  },
+
 };
