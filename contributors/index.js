a5b5f59b188997fa3402a01d58721ba6b3a69e23
Saeed <saeed.rezaei.sa@gmail.com>
2019-10-09 17:55:22 +0330

    Saeed's card

--- contributors/index.js
+++ contributors/index.js
@@ -193,5 +193,13 @@ module.exports.list = [
     tech_stack: ["Angular", "Node", "Express"],
     github_username: "grvv",
     twitter_username: "#"
+  },
+  {
+    name: "Saeed",
+    country: "Iran",
+    role: "To be filled",
+    tech_stack: ["Android", "iOS", "Node"],
+    github_username: "saeed-rz",
+    twitter_username: "@SaeedRezaeii"
   }
 ];
