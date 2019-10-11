793bafe1fc76707ca64a59f16d043d6dd59df5d4
RocktimSaikia <srocktim61@gmail.com>
2019-10-11 12:08:14 -0700

    Procfile for heroku deploymnent

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const { list } = require("./contributors/index");
 
 const express = require("express");
 const app = express();
+const reload = require("reload");
+const http = require("http");
 
+const server = http.createServer(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -10,8 +13,14 @@ app.get("/", (req, res) => {
   res.render("index", { List: list });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`connected to port ${port}`);
-});
+reload(app)
+  .then(function(reloadReturned) {
+    server.listen(port, function() {
+      console.log(`Server connected at port ${port}`);
+    });
+  })
+  .catch(function(err) {
+    console.err("Reload error occured: ", err);
+  });
