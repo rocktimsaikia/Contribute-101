d38890cf5db28c1c26e050d53df325eb68555308
RocktimSaikia <srocktim61@gmail.com>
2019-10-12 11:27:18 -0700

    unit testing with mocha chai and supertest

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../server');
+const { list } = require('../contributors/index');
+
+describe('unit testing the base route', () => {
+  it('should return Ok status', () =>
+    request(app)
+      .get('/')
+      .then(res => {
+        assert.equal(res.status, 200);
+      }));
+  it('should return type array', () => {
+    expect(list).to.be.an('array');
+  });
+});
