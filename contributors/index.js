461b00c422584e36b600cc18115c0ee8b1492d0d
jitesh101.kumar@gmail.com <jitesh101.kumar@gmail.com>
2019-10-12 17:45:12 +0530

    Urmilla's card

--- contributors/index.js
+++ contributors/index.js
@@ -1,5 +1,13 @@
 module.exports.list = [
-  {
+{
+    name: 'Urmilla',
+    country: 'India',
+    role: 'Undergraduate',
+    tech_stack: ['Flutter', 'JavaScript', 'Express'],
+    github_username: 'urmilla20',
+    twitter_username: '#',
+  }, 
+ {
     name: 'Jitesh',
     country: 'India',
     role: 'Undergraduate',
