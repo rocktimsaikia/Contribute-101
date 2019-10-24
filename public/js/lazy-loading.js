7250b7d820e16a669821413a23ad4d8e0f968ed7
RocktimSaikia <srocktim61@gmail.com>
2019-10-24 05:44:58 -0700

    lazy-loading on all images for faster load speed

--- public/js/lazy-loading.js
+++ public/js/lazy-loading.js
@@ -1,32 +1,35 @@
-!(function (window) {
-  const $q = function (q, res) {
+/* eslint-disable */
+
+!(function(window) {
+  const $q = function(q, res) {
     if (document.querySelectorAll) {
       res = document.querySelectorAll(q);
     } else {
       const d = document;
       const a = d.styleSheets[0] || d.createStyleSheet();
       a.addRule(q, 'f:b');
-      for (var l = d.all, b = 0, c = [], f = l.length; b < f; b++) l[b].currentStyle.f && c.push(l[b]);
+      for (var l = d.all, b = 0, c = [], f = l.length; b < f; b++)
+        l[b].currentStyle.f && c.push(l[b]);
 
       a.removeRule(0);
       res = c;
     }
     return res;
   };
-  const addEventListener = function (evt, fn) {
+  const addEventListener = function(evt, fn) {
     window.addEventListener
       ? this.addEventListener(evt, fn, false)
       : window.attachEvent
-        ? this.attachEvent(`on${evt}`, fn)
-        : (this[`on${evt}`] = fn);
+      ? this.attachEvent(`on${evt}`, fn)
+      : (this[`on${evt}`] = fn);
   };
-  const _has = function (obj, key) {
+  const _has = function(obj, key) {
     return Object.prototype.hasOwnProperty.call(obj, key);
   };
   function loadImage(el, fn) {
     const img = new Image();
     const src = el.getAttribute('data-src');
-    img.onload = function () {
+    img.onload = function() {
       if (el.parent) el.parent.replaceChild(img, el);
       else el.src = src;
 
@@ -39,15 +42,15 @@
     const rect = el.getBoundingClientRect();
 
     return (
-      rect.top >= 0
-      && rect.left >= 0
-      && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)
     );
   }
 
   const images = new Array();
   const query = $q('img.lazy');
-  const processScroll = function () {
+  const processScroll = function() {
     for (var i = 0; i < images.length; i++) {
       if (elementInViewport(images[i])) {
         loadImage(images[i], () => {
@@ -63,4 +66,4 @@
 
   processScroll();
   addEventListener('scroll', processScroll);
-}(this));
+})(this);
