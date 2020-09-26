a677802032c9ce14a0e3f5600c9f8019b404882d
rocktimsaikia <srocktim61@gmail.com>
2020-09-26 23:28:10 +0530

    chore: reverse the list array in the export statement itself.
    fix the issue of reversing of the list on every page refresh

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const reload = require('reload');
 const http = require('http');
 const {
-  list
+	list: List
 } = require('./contributors/index');
 
 const server = http.createServer(app);
@@ -12,21 +12,21 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    List: list.reverse()
-  });
+	res.render('index', {
+		List
+	});
 });
 
 const port = process.env.PORT || 3000;
 
 reload(app)
-  .then(() => {
-    server.listen(port, () => {
-      console.log(`Server connected at port ${port}`);
-    });
-  })
-  .catch(err => {
-    console.err('Reload error occurred: ', err);
-  });
+	.then(() => {
+		server.listen(port, () => {
+			console.log(`Server connected at port ${port}`);
+		});
+	})
+	.catch(err => {
+		console.err('Reload error occurred: ', err);
+	});
 
 module.exports = app;
