2217d6c7f4257768b9d345b9c6a8bacee6528489
Ramanujam Gond <ramanujamgond@gmail.com>
2020-09-30 14:25:00 +0530

    Add Ramanujam's card (#268)

--- contributors/index.js
+++ contributors/index.js
@@ -1414,6 +1414,14 @@ const list = [{
 		github_username: 'cat-street',
 		twitter_username: 'cat__logic',
 	},
+	{
+		name: 'Ramanujam Gond',
+		country: 'India',
+		role: 'UI/UX Developer',
+		tech_stack: ['JavaScript', 'ReactJs', 'Dart', 'C++'],
+		github_username: 'ramanujamgond',
+		twitter_username: 'string1301',
+	},
 ];
 
 module.exports.list = list.reverse();
